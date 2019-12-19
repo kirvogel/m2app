654be26f96681555ab2bc7b10758ae9b3969e2f4
Natalia Shashok <nshashok@g.nsu.ru>
2019-12-19 12:22:58 +0700

    [TOWERS] Mountry codes + mobile network codes (made static)

--- map_2_app/src/main/java/com/example/data/MobileCountryCodeMobileNetworkCode.java
+++ map_2_app/src/main/java/com/example/data/MobileCountryCodeMobileNetworkCode.java
@@ -7,17 +7,20 @@ import java.util.Map;
 
 public class MobileCountryCodeMobileNetworkCode {
     @SuppressLint("UseSparseArrays")
-    private Map<Integer, int[]> map = new HashMap<>();
+    private static Map<Integer, int[]> map = new HashMap<>();
 
-    MobileCountryCodeMobileNetworkCode() {
+    private MobileCountryCodeMobileNetworkCode() {
+
+    }
+
+    static {
         map.put(289, new int[]{67, 68}); //абхазия
-
         map.put(250, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 19, 20,
         23, 28, 32, 35, 37, 38, 39, 44, 50, 92, 93, 99}); //россия
 
     }
 
-    public int[] getMNCList(int mcc) {
+    public static int[] getMNCList(int mcc) {
         try {
             return map.get(mcc);
         } catch (Exception e) {
