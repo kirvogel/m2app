340384e4abddc2470310add5acc429d64ee79990
Natalia Shashok <nshashok@g.nsu.ru>
2019-12-20 17:36:16 +0700

    [TESTS] added PowerMock

--- map_2_app/src/main/java/com/example/data/MobileCountryCodeMobileNetworkCode.java
+++ map_2_app/src/main/java/com/example/data/MobileCountryCodeMobileNetworkCode.java
@@ -1,90 +1,28 @@
 package com.example.data;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.telephony.TelephonyManager;
 import android.telephony.gsm.GsmCellLocation;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 import timber.log.Timber;
 
-import static com.mapbox.mapboxsdk.Mapbox.getApplicationContext;
-
 public class MobileCountryCodeMobileNetworkCode {
-    @SuppressLint("UseSparseArrays")
-    private static Map<Integer, int[]> map = new HashMap<>();
-    private static Map<String, Integer> mapNames = new HashMap<>();
 
     private MobileCountryCodeMobileNetworkCode() {
 
     }
 
-    static {
-        mapNames.put("ru", 250);
-        map.put(250, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 19, 20,
-        23, 28, 32, 35, 37, 38, 39, 44, 50, 92, 93, 99}); //россия
-
-    }
-
-    public static int[] getMNCList(int mcc) {
-        try {
-            return map.get(mcc);
-        } catch (Exception e) {
-            return new int[]{};
-        }
-    }
-
-    public static Integer getMCC(String name) {
-        if (name == null) return 0;
-        try {
-            return mapNames.get(name);
-        } catch (NullPointerException e) {
-            return 0;
-        }
-    }
-
-    public static String getCountryCode(double latitude, double longtitude) throws IOException {
-        URL url = new URL("https://us1.unwiredlabs.com/v2/reverse.php?token="
-                + "a65aee3fdcc744"
-                + "&lat=" + latitude
-                + "&lon=" + longtitude);
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) {
-            String inputLine;
-            StringBuilder response = new StringBuilder();
-
-            while ((inputLine = bufferedReader.readLine()) != null) {
-                response.append(inputLine);
-            }
-            return response.toString();
-        } catch (IOException e) {
-            Timber.tag("M2APP").v(e);
-        } finally {
-            urlConnection.disconnect();
-        }
-        return "";
-    }
-
     @SuppressLint("Assert")
     public static String getCoutryName(double latitude, double longtitude) {
         try  {
-            JSONObject json = new JSONObject(getCountryCode(latitude, longtitude));
+            JSONObject json = new JSONObject(Requests.getCountryCode(latitude, longtitude));
             return (String)((JSONObject)json.get("address")).get("country_code");
         } catch (IOException | JSONException e) {
             Timber.tag("M2APP").v(e);
@@ -92,60 +30,13 @@ public class MobileCountryCodeMobileNetworkCode {
         return "";
     }
 
-    public static String[] getStations(double latitude, double longtitude, int cellId, int lac, String country) throws IOException {
-        Integer code = getMCC(country);
-        int[] mncList = getMNCList(code);
-        List<String> resList = new LinkedList<>();
 
-        for (int mnc :
-                mncList) {
-            URL url = new URL("https://us1.unwiredlabs.com/v2/process.php");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setDoOutput(true);
-            connection.setRequestMethod("POST");
-            connection.setReadTimeout(10000);
-            try (Writer writer = new OutputStreamWriter(connection.getOutputStream());
-                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                String query = "{\n" +
-                        "    \"token\": \"a65aee3fdcc744\"," +
-                        "    \"radio\": \"gsm\"," +
-                        "    \"mcc\": " + code + "," +
-                        "    \"mnc\": " + mnc + "," +
-                        "    \"cells\": [{" +
-                        "        \"cid\": " + cellId + "," +
-                        "\"lac\":" + lac +
-                        "    }]," +
-                        "    \"address\": 0" +
-                        "}";
-
-                writer.write(query);
-                writer.flush();
-
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-
-                while ((inputLine = bufferedReader.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                resList.add(response.toString());
-            } catch (IOException e) {
-                Timber.tag("M2APP").v(e);
-            } finally {
-                connection.disconnect();
-            }
-        }
-        String[] objects = new String[resList.size()];
-        for (int i = 0; i < resList.size(); ++i) {
-            objects[i] = resList.get(i);
-        }
-        return objects;
-    }
 
     @SuppressLint("Assert")
     public static JSONObject[] getStations(double latitude, double longtitude) throws IOException {
 
-        MyResult res = getCellId();
-        String[] stations = getStations(latitude, longtitude, res.getFirst(),res.getSecond(), getCoutryName(latitude, longtitude));
+        MyResult res = Requests.getCellId();
+        String[] stations = Requests.getStations(res.getFirst(),res.getSecond(), getCoutryName(latitude, longtitude));
         List<JSONObject> obj = new LinkedList<>();
 
         for (String mnc :
@@ -167,23 +58,4 @@ public class MobileCountryCodeMobileNetworkCode {
         return objects;
     }
 
-    private static MyResult getCellId() {
-        int cellId = 0;
-        int lac = 0;
-        final TelephonyManager telephony = getManager();
-        assert telephony != null;
-        if (telephony.getPhoneType() == TelephonyManager.PHONE_TYPE_GSM) {
-            @SuppressLint("MissingPermission") final GsmCellLocation location = (GsmCellLocation) telephony.getCellLocation();
-            if (location != null) {
-                cellId = location.getCid();
-                lac = location.getLac();
-            }
-        }
-        return new MyResult(cellId, lac);
-    }
-
-    private static TelephonyManager getManager() {
-        return
-                (TelephonyManager) getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE);
-    }
 }
--- map_2_app/src/test/java/com/example/m2app/ExampleUnitTest.java
+++ map_2_app/src/test/java/com/example/m2app/ExampleUnitTest.java
@@ -1,21 +1,40 @@
 package com.example.m2app;
 
-import com.example.data.MobileCountryCodeMobileNetworkCode;
+import android.content.Context;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
+import android.test.mock.MockContext;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
 import com.example.data.MyResult;
+import com.example.data.Requests;
+import com.mapbox.mapboxsdk.Mapbox;
 
 import org.junit.Test;
+import org.junit.runner.Request;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.MockitoAnnotations;
+import org.mockito.MockitoSession;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.IOException;
 
+import static com.mapbox.mapboxsdk.Mapbox.getApplicationContext;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
-//import androidx.test.platform.app.InstrumentationRegistry;
-//
+/*
+import androidx.test.platform.app.InstrumentationRegistry;
+
+*/
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -23,7 +42,7 @@ import static org.junit.Assert.assertTrue;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 //@RunWith(RobolectricTestRunner.class)
-    @RunWith(JUnit4.class)
+@RunWith(PowerMockRunner.class)
 public class ExampleUnitTest {
     @Test
     //@Config(sdk = Build.VERSION_CODES.O_MR1)
@@ -41,25 +60,25 @@ public class ExampleUnitTest {
     }
     //@Test
     //@Config(sdk = Build.VERSION_CODES.O_MR1)
-    /*public void useAppContext() {
+    //public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.m2app", appContext.getPackageName());
-    }*/
+      //  Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+     //   assertEquals("com.example.m2app", appContext.getPackageName());
+    //}
     @Test
    // @Config(sdk = Build.VERSION_CODES.O_MR1)
     public void mobileProvidersCodeTest() {
-        assertNotEquals(0, MobileCountryCodeMobileNetworkCode.getMNCList(250).length);
+        assertNotEquals(0, Requests.getMNCList(250).length);
     }
 
     @Test
     // @Config(sdk = Build.VERSION_CODES.O_MR1)
     public void mobileCountryCodeTest() {
-        assertEquals(250, (int)MobileCountryCodeMobileNetworkCode.getMCC("ru"));
+        assertEquals(250, (int)Requests.getMCC("ru"));
     }
     @Test
     public void MobileCountryNameTest() throws IOException {
-        String code = MobileCountryCodeMobileNetworkCode.getCountryCode(55.751244, 37.618423);
+        String code = Requests.getCountryCode(55.751244, 37.618423);
         assertTrue(code
                 .startsWith("{\"status\":\"ok\""));
         assertTrue(code.endsWith("}}"));
@@ -67,9 +86,34 @@ public class ExampleUnitTest {
 
     @Test
     public void StationsTest() throws IOException {
-        String[] getStations = MobileCountryCodeMobileNetworkCode.getStations(
-                55.751244, 37.618423,
+        String[] getStations = Requests.getStations(
                 2224, 16, "ru");
         assertNotNull(getStations);
+    }
+
+    @Test
+    @PrepareForTest({Mapbox.class})
+    public void CellIdTest() {
+
+        MockitoAnnotations.initMocks(this);
+        PowerMockito.mockStatic(Mapbox.class);
+        int type = TelephonyManager.PHONE_TYPE_GSM;
+
+        TelephonyManager mockTelephonyManager = mock(TelephonyManager.class);
+        GsmCellLocation location = mock(GsmCellLocation.class);
+
+        when(location.getCid()).thenReturn(0);
+        when(location.getLac()).thenReturn(0);
+        when(mockTelephonyManager.getPhoneType()).thenReturn(type);
+        when(mockTelephonyManager.getCellLocation()).thenReturn(location);
+
+        MockContext mockContext =  mock(MockContext.class);
+        when(mockContext.getSystemService(any(String.class))).thenReturn(mockTelephonyManager);
+
+        when(Mapbox.getApplicationContext()).thenReturn(mockContext);
+
+        MyResult res = Requests.getCellId();
+        assertEquals(0, res.getFirst());
+        assertEquals(0, res.getSecond());
     }
 }
