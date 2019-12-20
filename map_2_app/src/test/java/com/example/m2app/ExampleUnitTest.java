b7306b1118a4edc450c22af85000b47d620f67f6
Natalia Shashok <nshashok@g.nsu.ru>
2019-12-20 13:50:37 +0700

    [TESTING] Trying to test with JaCoCo (6% coverage)

--- map_2_app/src/test/java/com/example/m2app/ExampleUnitTest.java
+++ map_2_app/src/test/java/com/example/m2app/ExampleUnitTest.java
@@ -1,37 +1,47 @@
 package com.example.m2app;
 
-import android.content.Context;
-import android.os.Build;
-
-import androidx.test.platform.app.InstrumentationRegistry;
-
 import com.example.data.MobileCountryCodeMobileNetworkCode;
 import com.example.data.MyResult;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+
+//import androidx.test.platform.app.InstrumentationRegistry;
+//
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+//@RunWith(RobolectricTestRunner.class)
 public class ExampleUnitTest {
     @Test
+    //@Config(sdk = Build.VERSION_CODES.O_MR1)
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
 
 
     @Test
+    //@Config(sdk = Build.VERSION_CODES.O_MR1)
     public void testMyResultClass() {
         MyResult myres = new MyResult(1, 2);
         assertEquals(1, myres.getFirst());
         assertEquals(2, myres.getSecond());
+    }
+    //@Test
+    //@Config(sdk = Build.VERSION_CODES.O_MR1)
+    /*public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.m2app", appContext.getPackageName());
+    }*/
+    @Test
+   // @Config(sdk = Build.VERSION_CODES.O_MR1)
+    public void mobileCoutryCodeTest() {
+        assertNotEquals(0, MobileCountryCodeMobileNetworkCode.getMNCList(250).length);
     }
 }
