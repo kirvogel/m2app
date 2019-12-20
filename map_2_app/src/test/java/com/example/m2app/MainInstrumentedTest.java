aa6661b241fbab8a631228f914929b1918910bd1
Natalia Shashok <nshashok@g.nsu.ru>
2019-12-21 01:45:28 +0700

    [TEST] Changed some tests behaviour

--- map_2_app/src/test/java/com/example/m2app/MainInstrumentedTest.java
+++ map_2_app/src/test/java/com/example/m2app/MainInstrumentedTest.java
@@ -1,25 +1,19 @@
 package com.example.m2app;
 
-import android.content.Context;
-
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-import org.robolectric.RobolectricTestRunner;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -27,7 +21,7 @@ import static org.junit.Assert.assertEquals;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class MainInstrumentedTest {
     @Rule
     public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
     @Before
