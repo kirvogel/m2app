81d585d5f5817daf4f0b3ec371d31929e50d0774
Natalia Shashok <nshashok@g.nsu.ru>
2019-12-20 17:42:56 +0700

    [REFACTOR] refactoring

--- map_2_app/src/main/java/com/example/m2app/MainActivity.java
+++ map_2_app/src/main/java/com/example/m2app/MainActivity.java
@@ -22,13 +22,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
         FloatingActionButton fab2 = findViewById(R.id.button);
         fab2.setOnClickListener(new View.OnClickListener() {
             @Override
